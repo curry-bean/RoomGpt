061d86837fab0c511cf66455ec1960acdbb9ecdf
curry-bean <curry.bean.017@gmail.com>
2023-03-04 08:36:11 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# [roomGPT.io](https://roomGPT.io)
+
+This project generates designs of your room with AI.
+
+[![Room GPT](./public/screenshot.png)](https://roomGPT.io)
+
+## How it works
+
+It uses an ML model called [ControlNet](https://github.com/lllyasviel/ControlNet) to generate variations of rooms. This application gives you the ability to upload a photo of any room, which will send it through this ML Model using a Next.js API route, and return your generated room. The ML Model is hosted on [Replicate](https://replicate.com) and [Upload](https://upload.io) is used for image storage.
+
+## Running Locally
+
+### Cloning the repository the local machine.
+
+```bash
+git clone https://github.com/Nutlope/roomGPT
+```
+
+### Creating a account on Replicate to get an API key.
+
+1. Go to [Replicate](https://replicate.com/) to make an account.
+2. Click on your profile picture in the top right corner, and click on "Dashboard".
+3. Click on "Account" in the navbar. And, here you can find your API token, copy it.
+
+### Storing the API keys in .env
+
+Create a file in root directory of project with env. And store your API key in it, as shown in the .example.env file.
+
+If you'd also like to do rate limiting, create an account on UpStash, create a Redis database, and populate the two environment variables in `.env` as well. If you don't want to do rate limiting, you don't need to make any changes.
+
+### Installing the dependencies.
+
+```bash
+npm install
+```
+
+### Running the application.
+
+Then, run the application in the command line and it will be available at `http://localhost:3000`.
+
+```bash
+npm run dev
+```
+
+## One-Click Deploy
+
+Deploy the example using [Vercel](https://vercel.com?utm_source=github&utm_medium=readme&utm_campaign=vercel-examples):
+
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https://github.com/Nutlope/roomGPT&env=REPLICATE_API_KEY&project-name=room-GPT&repo-name=roomGPT)
---
+++ pages/api/generate.ts
@@ -0,0 +1,97 @@
+import { Ratelimit } from "@upstash/ratelimit";
+import type { NextApiRequest, NextApiResponse } from "next";
+import requestIp from "request-ip";
+import redis from "../../utils/redis";
+
+type Data = string;
+interface ExtendedNextApiRequest extends NextApiRequest {
+  body: {
+    imageUrl: string;
+    theme: string;
+    room: string;
+  };
+}
+
+// Create a new ratelimiter, that allows 5 requests per 24 hours
+const ratelimit = redis
+  ? new Ratelimit({
+      redis: redis,
+      limiter: Ratelimit.fixedWindow(5, "1440 m"),
+      analytics: true,
+    })
+  : undefined;
+
+export default async function handler(
+  req: ExtendedNextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  // Rate Limiter Code
+  if (ratelimit) {
+    const identifier = requestIp.getClientIp(req);
+    const result = await ratelimit.limit(identifier!);
+    res.setHeader("X-RateLimit-Limit", result.limit);
+    res.setHeader("X-RateLimit-Remaining", result.remaining);
+
+    if (!result.success) {
+      res
+        .status(429)
+        .json("Too many uploads in 1 day. Please try again in a 24 hours.");
+      return;
+    }
+  }
+
+  const { imageUrl, theme, room } = req.body;
+  // POST request to Replicate to start the image restoration generation process
+  let startResponse = await fetch("https://api.replicate.com/v1/predictions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Token " + process.env.REPLICATE_API_KEY,
+    },
+    body: JSON.stringify({
+      version:
+        "854e8727697a057c525cdb45ab037f64ecca770a1769cc52287c2e56472a247b",
+      input: {
+        image: imageUrl,
+        prompt:
+          room === "Gaming Room"
+            ? "a room for gaming with gaming computers, gaming consoles, and gaming chairs"
+            : `a ${theme.toLowerCase()} ${room.toLowerCase()}`,
+        a_prompt:
+          "best quality, extremely detailed, photo from Pinterest, interior, cinematic photo, ultra-detailed, ultra-realistic, award-winning",
+        n_prompt:
+          "longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality",
+      },
+    }),
+  });
+
+  let jsonStartResponse = await startResponse.json();
+
+  let endpointUrl = jsonStartResponse.urls.get;
+
+  // GET request to get the status of the image restoration process & return the result when it's ready
+  let restoredImage: string | null = null;
+  while (!restoredImage) {
+    // Loop in 1s intervals until the alt text is ready
+    console.log("polling for result...");
+    let finalResponse = await fetch(endpointUrl, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Token " + process.env.REPLICATE_API_KEY,
+      },
+    });
+    let jsonFinalResponse = await finalResponse.json();
+
+    if (jsonFinalResponse.status === "succeeded") {
+      restoredImage = jsonFinalResponse.output;
+    } else if (jsonFinalResponse.status === "failed") {
+      break;
+    } else {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+  }
+  res
+    .status(200)
+    .json(restoredImage ? restoredImage : "Failed to restore image");
+}
